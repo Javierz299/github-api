07e536ba6825be8e53b26a340a8f467e19cf2af0
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-07 13:28:13 -0400

    cancel request clean up and err handling

--- src/Hooks/useFetchJobs.js
+++ src/Hooks/useFetchJobs.js
@@ -11,13 +11,25 @@ const useFetchJobs = (params,page) => {
     const [state, dispatch] = useReducer(JobsReducer, {jobs: [], loading: false, error: false})
 
     useEffect(() => {
+        //whenever params change cancel prev request
+        const cancelToken = axios.CancelToken.source();
         dispatch(ACTIONS.make_request())
         axios.get(`${BASE_URL}`,{
+            cancelToken: cancelToken.token,
             params: {markdown: true, page: page, ...params}
         })
         .then(res => dispatch(ACTIONS.get_data(res.data)))
-        .catch((err) => dispatch(ACTIONS.error({error: err})))
+        .catch((err) => {
+            //when request is canceled err will be thrown
+            //only catch unintential errors
+            if(axios.isCancel(err)) return;
+            dispatch(ACTIONS.error({error: err}))
+        });
 
+        //clean-up prev request
+        return () => {
+            cancelToken.cancel()
+        }
 
     }, [params,page])
    
